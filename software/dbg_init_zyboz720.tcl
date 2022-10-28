e3cc1e7845b0081b84229b624595006066e792de
brac24-UbuntuHome <2brac4@gmail.com>
2022-10-28 00:25:45 -0700

    had to run vitis to generate the tcl script used to program the board. I should not track this file since it is machine specific

--- software/dbg_init_zyboz720.tcl
+++ software/dbg_init_zyboz720.tcl
@@ -3,14 +3,14 @@ targets -set -nocase -filter {name =~"APU*"}
 rst -system
 after 3000
 targets -set -filter {jtag_cable_name =~ "Digilent Zybo Z7 210351B5C102A" && level==0 && jtag_device_ctx=="jsn-Zybo Z7-210351B5C102A-23727093-0"}
-fpga -file /home/brac24/Repos/zyboz7-vscode-debug-test/software/test-app/_ide/bitstream/design_1_wrapper.bit
+fpga -file /home/brac24/zybo-z720-vscode-template/software/test-app/_ide/bitstream/design_1_wrapper.bit
 targets -set -nocase -filter {name =~"APU*"}
-loadhw -hw /home/brac24/Repos/zyboz7-vscode-debug-test/software/design_1_wrapper/export/design_1_wrapper/hw/design_1_wrapper.xsa -mem-ranges [list {0x40000000 0xbfffffff}] -regs
+loadhw -hw /home/brac24/zybo-z720-vscode-template/software/design_1_wrapper/export/design_1_wrapper/hw/design_1_wrapper.xsa -mem-ranges [list {0x40000000 0xbfffffff}] -regs
 configparams force-mem-access 1
 targets -set -nocase -filter {name =~"APU*"}
-source /home/brac24/Repos/zyboz7-vscode-debug-test/software/test-app/_ide/psinit/ps7_init.tcl
+source /home/brac24/zybo-z720-vscode-template/software/test-app/_ide/psinit/ps7_init.tcl
 ps7_init
 ps7_post_config
 targets -set -nocase -filter {name =~ "*A9*#0"}
-dow /home/brac24/Repos/zyboz7-vscode-debug-test/software/test-app/Debug/test-app.elf
+dow /home/brac24/zybo-z720-vscode-template/software/test-app/Debug/test-app.elf
 configparams force-mem-access 0
